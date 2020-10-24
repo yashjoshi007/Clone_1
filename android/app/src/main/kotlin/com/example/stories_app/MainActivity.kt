83cf0c6acf3823ede79316a0d70975e7ce3a14e9
yashjoshi007 <yashjoshi785@gmail.com>
2020-10-24 19:03:49 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/stories_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stories_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/story_model.dart
@@ -0,0 +1,22 @@
+import 'package:meta/meta.dart';
+
+import 'package:stories_app/models/user_model.dart';
+
+enum MediaType {
+  image,
+  video,
+}
+
+class Story {
+  final String url;
+  final MediaType media;
+  final Duration duration;
+  final User user;
+
+  const Story({
+    @required this.url,
+    @required this.media,
+    @required this.duration,
+    @required this.user,
+  });
+}
